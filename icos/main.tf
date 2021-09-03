095304b371f7a296963122ac737b6b7b3dfad911
Shinya-Yoshizawa <yoshizawa@1excellence.com>
2021-09-03 16:31:17 +0900

    first

---
+++ icos/main.tf
@@ -0,0 +1,19 @@
+data "ibm_resource_group" "cos_group" {
+  name = var.resource_group_name
+}
+resource "ibm_resource_instance" "cos_instance" {
+  name              = var.cos_instance_name
+  resource_group_id = data.ibm_resource_group.cos_group.id
+  service           = "cloud-object-storage"
+  plan              = "lite"
+  location          = "global"
+}
+
+resource "ibm_cos_bucket" "backet" {
+  bucket_name           = var.bucket_name
+  resource_instance_id  = ibm_resource_instance.cos_instance.id
+  single_site_location  = var.single_site_loc
+  storage_class         = var.storage
+  hard_quota            = var.quota
+}
+
---
+++ icos/variables.tf
@@ -0,0 +1,27 @@
+variable "cos_instance_name" {
+  default = "yoshizawa-icos-poc"
+}
+
+variable "bucket_name" {
+  default = "yoshizawa-icos-poc-webservice"
+}
+
+variable "resource_group_name" {
+  default = "Default"
+}
+
+variable "storage" {
+  default = "smart tier"
+}
+
+variable "region" {
+  default = "ap"
+}
+
+variable "single_site_loc" {
+  default = "jp-osa"
+}
+
+variable "quota" {
+  default = "0"
+}
