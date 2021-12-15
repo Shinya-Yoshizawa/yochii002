8638663376215cfd3f794a2d4fe777f2a55a2fe7
yoshizawa@1excellence.com <yoshizawa@1excellence.com>
2021-12-15 13:43:54 +0900

    main.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,31 @@
+variable "zone1" {
+  default = "us-south-1"
+}
+
+variable "zone2" {
+  default = "us-south-2"
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "image" {
+  default = "r006-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+}
+
+variable "profile" {
+  default = "cx2-2x4"
+}
+
+variable "image_cos_url" {
+  default = "cos://us-south/cosbucket-vpc-image-gen2/rhel-guest-image-7.0-encrypted.qcow2"
+}
+
+variable "image_operating_system" {
+  default = "red-7-amd64"
+}
+
+variable "cidr1" {
+  default = "10.120.0.0/24"
+}
