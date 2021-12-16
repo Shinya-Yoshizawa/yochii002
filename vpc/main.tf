b603694d4fca799e1f77742a8b1f7a04505cebcb
yoshizawa@1excellence.com <yoshizawa@1excellence.com>
2021-12-16 18:38:58 +0900

    05

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,7 +3,7 @@ resource "ibm_is_vpc" "vpc1" {
   resource_group = var.resource_group
 }
 
-resource "ibm_is_vpc_address_prefix" "zone1_sabnet_prefix" {
+resource "ibm_is_vpc_address_prefix" "zone1" {
   name        = var.zone1_subnet_prefix_name
   zone        = var.zone1
   vpc         = ibm_is_vpc.vpc1.id
@@ -11,47 +11,47 @@ resource "ibm_is_vpc_address_prefix" "zone1_sabnet_prefix" {
 	is_default  = true
 }
 
-resource "ibm_is_vpc_address_prefix" "zone2_sabnet_prefix" {
+resource "ibm_is_vpc_address_prefix" "zone2" {
   name        = var.zone2_subnet_prefix_name
   zone        = var.zone2
   vpc         = ibm_is_vpc.vpc1.id
 	cidr        = var.cidr1
 }
 
-resource "ibm_is_vpc_address_prefix" "zone3_sabnet_prefix" {
+resource "ibm_is_vpc_address_prefix" "zone3" {
   name        = var.zone3_subnet_prefix_name
   zone        = var.zone3
   vpc         = ibm_is_vpc.vpc1.id
 	cidr        = var.cidr1
 }
 
-resource "ibm_is_vpc_route" "route" {
-  name        = "route1"
-  vpc         = ibm_is_vpc.vpc1.id
-  zone        = var.zone1
-  destination = "192.168.0.0/24"
-  next_hop    = "10.248.0.1"
-  depends_on  = [ibm_is_subnet.subnet1]
-}
+#resource "ibm_is_vpc_route" "route" {
+#  name        = "route1"
+#  vpc         = ibm_is_vpc.vpc1.id
+#  zone        = var.zone1
+#  destination = "192.168.0.0/24"
+#  next_hop    = "10.248.0.1"
+#  depends_on  = [ibm_is_subnet.subnet1]
+#}
 
 resource "ibm_is_subnet" "subnet1" {
   name            = var.subnet_name
   vpc             = ibm_is_vpc.vpc1.id
-  zone            = [var.zone1]
+  zone            = ibm_is_vpc_address_prefix.zone1
   ipv4_cidr_block = "10.248.0.0/28"
 }
 
 resource "ibm_is_subnet" "subnet2" {
   name            = var.subnet_name
   vpc             = ibm_is_vpc.vpc1.id
-  zone            = [var.zone2]
+  zone            = ibm_is_vpc_address_prefix.zone2
   ipv4_cidr_block = "10.248.48.0/28"
 }
 
 resource "ibm_is_subnet" "subnet3" {
   name            = var.subnet_name
   vpc             = ibm_is_vpc.vpc1.id
-  zone            = [var.zone3]
+  zone            = ibm_is_vpc_address_prefix.zone3
   ipv4_cidr_block = "10.248.96.0/28"
 }
 
