c5d230f1f1d976d2b831d15b2673e8bcf3de34a5
yoshizawa@1excellence.com <yoshizawa@1excellence.com>
2021-12-23 10:20:07 +0900

    07

--- vpc/main.tf
+++ vpc/main.tf
@@ -23,26 +23,26 @@ resource ibm_is_security_group sg_def {
 }
 
 # Default Security Group Rule
-resource ibm_is_security_group_rule sg_rule_tcp22 {
-  group     = ibm_is_security_group.sg_def.id
-  direction = "inbound"
-  remote    = "0.0.0.0/0"
-  tcp {
-    port_min = 22
-    port_max = 22
-  }
-}
-resource ibm_is_security_group_rule sg_rule_ping {
-  group     = ibm_is_security_group.sg_def.id
-  direction = "inbound"
-  remote    = "0.0.0.0/0"
-  icmp {
-    type = 8
-    code = 0
-  }
-}
-resource ibm_is_security_group_rule sg_rule_egress {
-  group     = ibm_is_security_group.sg_def.id
-  direction = "egress"
-  remote    = "0.0.0.0/0"
-}
+#resource ibm_is_security_group_rule sg_rule_tcp22 {
+#  group     = ibm_is_security_group.sg_def.id
+#  direction = "inbound"
+#  remote    = "0.0.0.0/0"
+#  tcp {
+#    port_min = 22
+#    port_max = 22
+#  }
+#}
+#resource ibm_is_security_group_rule sg_rule_ping {
+#  group     = ibm_is_security_group.sg_def.id
+#  direction = "inbound"
+#  remote    = "0.0.0.0/0"
+#  icmp {
+#    type = 8
+#    code = 0
+#  }
+#}
+#resource ibm_is_security_group_rule sg_rule_egress {
+#  group     = ibm_is_security_group.sg_def.id
+#  direction = "egress"
+#  remote    = "0.0.0.0/0"
+#}
